170ed3121e315afa6295819e9377109ff97268bd
janvidamor <100026018+janvidamor@users.noreply.github.com>
2022-02-20 09:45:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d")
+img_width = 600;
+img_height = 200;
+
+var img_image;
+
+img_x = 200;
+img_y = 200;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+window.addEventListener("keydown,key_down");
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		//write a code to check the type of key pressed
+	
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+
+}
+
+function aplhabetkey()
+{
+	//upload respective image with the message. 
+
+}
+function numberkey()
+{
+	
+}
+function arrowkey()
+{
+}
+function specialkey()
+{
+	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
